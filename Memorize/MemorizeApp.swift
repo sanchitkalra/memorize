c770bc265502a32d5825b50c16df1b79625c2bf0
Sanchit Kalra <sanchit.kalra1@gmail.com>
2021-12-08 19:32:54 +0530

    Add some more notes for let 4

--- Memorize/MemorizeApp.swift
+++ Memorize/MemorizeApp.swift
@@ -10,11 +10,11 @@ import SwiftUI
 @main
 struct MemorizeApp: App {
     
-    let game = EmojiMemoryGame()
+    let game = EmojiMemoryGame() // this is an instance of the viewmodel that is then passed down to the content view which is the main view for our app.
     
     var body: some Scene {
         WindowGroup {
-            ContentView(viewModel: game)
+            ContentView(viewModel: game) // this var (viewModel) was uninitialised in the ContentView, hence needs to be passed here.
         }
     }
 }
